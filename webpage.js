ed530346ca2962e2d0f27675877f925c96823aea
JacquelinD <doranjacquelin@gmail.com>
2023-07-22 11:03:43 -0400

    added button with js

--- webpage.js
+++ webpage.js
@@ -1,4 +1,21 @@
-let text = document.getElementById("one");
+let body = document.getElementsByTagName("body");
+window.console.log(body);
 
+let element = document.getElementById("display-style");
+element.innerHTML = "CHANGED";
 
-text.innerText("I enjoy Cooking");
+let text = ["Taste", "Smell", "Sound", "Feel", "Hear"];
+let i = 0;
+
+document.getElementById("button").addEventListener("click", myFunction);
+
+function myFunction(){
+    if(i < text.length){
+        element.innerHTML = text[i];
+        i++;
+        window.console.log(i);
+    } else {
+        i = 0;
+        element.innerHTML = text[i];
+    }
+}
